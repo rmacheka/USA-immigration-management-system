16c7ba9814f5c2d4ddc1ae8015481c31258ba051
Ade Solanke <adesolanke@Ades-MacBook-Pro.local>
2025-05-14 09:18:54 -0700

    security: replace python-jose with PyJWT[crypto] to fix vulnerabilities

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "fastapi",
-        "PyJWT",
-        "python-jose[cryptography]",
+        "PyJWT[crypto]",
         "passlib[bcrypt]",
         "sqlalchemy",
     ],
